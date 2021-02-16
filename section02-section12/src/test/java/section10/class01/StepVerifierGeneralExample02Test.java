e89d5e490d601c76775eb7d482aa294cfab1b69e
hjs6877 <hjs6877@gmail.com>
2021-02-16 11:33:32 +0900

    .

--- section02-section12/src/test/java/section10/class01/StepVerifierGeneralExample02Test.java
+++ section02-section12/src/test/java/section10/class01/StepVerifierGeneralExample02Test.java
@@ -4,6 +4,9 @@ import com.itvillage.section10.class01.StepVerifierGeneralExample;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
+/***
+ * expectNext()를 사용하여 emit 된 n 개의 데이터를 검증하는 예제
+ */
 public class StepVerifierGeneralExample02Test {
     @Test
     public void sayHelloReactorTest() {
