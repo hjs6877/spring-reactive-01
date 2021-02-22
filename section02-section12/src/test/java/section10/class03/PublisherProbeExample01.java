92098801f0b4fc8b362da2198acb8567bc972a96
hjs6877 <hjs6877@gmail.com>
2021-02-22 23:49:09 +0900

    Implementing PublisherProbe Examples

--- section02-section12/src/test/java/section10/class03/PublisherProbeExample01.java
+++ section02-section12/src/test/java/section10/class03/PublisherProbeExample01.java
@@ -1,10 +1,26 @@
 package section10.class03;
 
+import com.itvillage.section10.class03.PublisherProbeExample;
+import com.itvillage.utils.Logger;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import reactor.test.publisher.PublisherProbe;
 
 public class PublisherProbeExample01 {
     @Test
     public void publisherProbeTest01() {
+        PublisherProbe<Void> probe = PublisherProbe.empty();
+
+        StepVerifier
+                .create(PublisherProbeExample.processTask(Mono.just("task01"), probe.mono()))
+                .verifyComplete();
+
+        probe.assertWasSubscribed();
+        probe.assertWasRequested();
+        probe.assertWasNotCancelled();
 
     }
 }
