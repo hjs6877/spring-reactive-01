6314a0f415def5fe5f62d29ba114a67a3595f284
hjs6877 <hjs6877@gmail.com>
2021-02-25 18:00:36 +0900

    Complete justOrEmpty examples

---
+++ section02-section12/src/main/java/com/itvillage/common/SampleData.java
@@ -0,0 +1,11 @@
+package com.itvillage.common;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SampleData {
+    public static final List<String> coins = Arrays.asList("BTX", "ETH", "XRP", "GRT", "EOS", "BCH");
+    private static class DataGenerator {
+
+    }
+}
