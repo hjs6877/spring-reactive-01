33c514dbb01698ca84583871d24e2c388af287ea
hjs6877 <hjs6877@gmail.com>
2021-03-09 17:34:25 +0900

    Complete fromStream examples

--- section02-section12/src/main/java/com/itvillage/section11/class01/FromStreamExample01.java
+++ section02-section12/src/main/java/com/itvillage/section11/class01/FromStreamExample01.java
@@ -5,13 +5,13 @@ import com.itvillage.utils.Logger;
 import reactor.core.publisher.Flux;
 
 /***
- * fromIterable()의 사용 예제
- *  - Iterable의 구현 클래스를 파라미터로 입력 받아 차례대로 emit한다.
+ * fromStream()의 사용 예제
+ *  - Stream을 파라미터로 입력 받아 Stream에 포함된 데이터를 차례대로 emit 한다.
  */
-public class FromIterableExample01 {
+public class FromStreamExample01 {
     public static void main(String[] args) {
         Flux
-                .fromIterable(SampleData.coinNames)
+                .fromStream(SampleData.coinNames.stream())
                 .subscribe(Logger::onNext);
     }
 }
