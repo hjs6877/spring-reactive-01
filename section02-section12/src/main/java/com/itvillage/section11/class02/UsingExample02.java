947236e0badd7b6d94fc2b1005f3ea8090c73ba2
hjs6877 <hjs6877@gmail.com>
2021-04-02 13:51:43 +0900

    Complete using examples

---
+++ section02-section12/src/main/java/com/itvillage/section11/class02/UsingExample02.java
@@ -0,0 +1,30 @@
+package com.itvillage.section11.class02;
+
+import com.itvillage.utils.Logger;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/**
+ * using()을 사용하기 적절한 예제
+ *  - 파라미터
+ *      - Callable(함수형 인터페이스): Resource를 input 으로 전달 받는다.(resource supplier)
+ *      - Function(함수형 인터페이스): input으로 전달받은 Resouce를 새로 생성한 Publisher로 emit한다.(source supplier)
+ *      - Consumer(함수형 인터페이스): 사용이 끝난 Resouce를 해제 또는 초기화 하는 등의 후처리를 한다.(resource cleanup)
+ */
+public class UsingExample02 {
+    public static void main(String[] args) {
+        Path path = Paths.get("D:\\resources\\using_example.txt");
+
+        Flux
+                .using(() -> Files.lines(path),
+                        Flux::fromStream,
+                        Stream::close
+                )
+                .subscribe(Logger::onNext);
+    }
+}
