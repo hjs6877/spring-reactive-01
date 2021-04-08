6f70edd9cdc1da5487600535dee5c39b9f215319
hjs6877 <hjs6877@gmail.com>
2021-04-08 18:44:44 +0900

    Add GenerateExample01,02

--- section02-section12/src/main/java/com/itvillage/section11/class02/UsingExample02.java
+++ section02-section12/src/main/java/com/itvillage/section11/class02/UsingExample02.java
@@ -12,7 +12,7 @@ import java.util.stream.Stream;
 /**
  * using()을 사용하기 적절한 예제
  *  - 파라미터
- *      - Callable(함수형 인터페이스): Resource를 input 으로 전달 받는다.(resource supplier)
+ *      - Callable(함수형 인터페이스): Resource를 input 으로 제공한다.(resource supplier)
  *      - Function(함수형 인터페이스): input으로 전달받은 Resouce를 새로 생성한 Publisher로 emit한다.(source supplier)
  *      - Consumer(함수형 인터페이스): 사용이 끝난 Resouce를 해제 또는 초기화 하는 등의 후처리를 한다.(resource cleanup)
  */
