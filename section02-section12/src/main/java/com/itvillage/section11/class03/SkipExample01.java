fbfac23bf06b82152ca7be7d63d82bc7c042322a
hjs6877 <hjs6877@gmail.com>
2021-06-30 16:21:35 +0900

    Implementing take operator's examples

--- section02-section12/src/main/java/com/itvillage/section11/class03/SkipExample01.java
+++ section02-section12/src/main/java/com/itvillage/section11/class03/SkipExample01.java
@@ -8,12 +8,13 @@ import java.time.Duration;
 
 /**
  * skip 기본 개념 예제
- *  - 파라미터로 입력한 숫자만큼 emit 될 데이터를 건너뛴 후, 건너뛴 다음 데이터부터 emit 한다.
+ *  - 파라미터로 입력한 숫자만큼 Upsteam에서 emit 되는 데이터를 건너뛴 후, 건너뛴 다음 데이터부터 Downstream으로 emit 한다.
  */
 public class SkipExample01 {
     public static void main(String[] args) {
         Flux
             .interval(Duration.ofSeconds(1))
+            .doOnNext(Logger::doOnNext)
             .skip(3)
             .subscribe(Logger::onNext);
 
--- section02-section12/src/main/java/com/itvillage/section11/class03/TakeExample02.java
+++ section02-section12/src/main/java/com/itvillage/section11/class03/TakeExample02.java
@@ -7,16 +7,17 @@ import reactor.core.publisher.Flux;
 import java.time.Duration;
 
 /**
- * skip 기본 개념 예제
- *  - 파라미터로 입력한 시간만큼 Upsteam에서 emit 되는 데이터를 건너뛴 후, 건너뛴 다음 데이터부터 Downstream으로 emit 한다.
+ * take 기본 개념 예제
+ *  - 파라미터로 입력한 시간 내에 Upstream에서 emit된 데이터만 Downstream으로 emit 한다.
  */
-public class SkipExample02 {
+public class TakeExample02 {
     public static void main(String[] args) {
         Flux
             .interval(Duration.ofSeconds(1))
-            .skip(Duration.ofSeconds(2))
+            .doOnNext(Logger::doOnNext)
+            .take(Duration.ofSeconds(2))
             .subscribe(Logger::onNext);
 
-        TimeUtils.sleep(5000L);
+        TimeUtils.sleep(3000L);
     }
 }
