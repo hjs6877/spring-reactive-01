dd9db4baad1c380d7dad35ab70ce3507b2d0a2a4
hjs6877 <hjs6877@gmail.com>
2021-07-29 23:47:19 +0900

    Implement FlatMapExample01

---
+++ section02-section12/src/main/java/com/itvillage/section11/class04/FlatMapExample02.java
@@ -0,0 +1,14 @@
+package com.itvillage.section11.class04;
+
+import com.itvillage.utils.Logger;
+import reactor.core.publisher.Flux;
+
+/**
+ * flatMap 기본 개념 예제
+ *  - 구구단 3단 출력 예제
+ */
+public class FlatMapExample02 {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
---
+++ section02-section12/src/main/java/com/itvillage/section11/class04/FlatMapExample03.java
@@ -0,0 +1,11 @@
+package com.itvillage.section11.class04;
+
+/**
+ * flatMap 기본 개념 예제
+ *  - 평탄화 과정을 거치기때문에 emit 되는 순서를 보장하지는 않는다.
+ */
+public class FlatMapExample03 {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
