30dc3091ef0ad2154d4a299e541a0b26074188e1
hjs6877 <hjs6877@gmail.com>
2021-08-23 23:42:32 +0900

    Implementing merge operator examples

--- section02-section12/src/main/java/com/itvillage/section11/class05/ConcatExample01.java
+++ section02-section12/src/main/java/com/itvillage/section11/class05/ConcatExample01.java
@@ -5,7 +5,7 @@ import reactor.core.publisher.Flux;
 
 /**
  * concat 기본 개념 예제
- *  - 파라미터로 입력된 Source Sequence 들을 연결해서 차례대로 emit한다.
+ *  - 파라미터로 입력된 Publisher Sequence 들을 연결해서 차례대로 emit한다.
  */
 public class ConcatExample01 {
     public static void main(String[] args) {
--- section02-section12/src/main/java/com/itvillage/section11/class05/ConcatExample02.java
+++ section02-section12/src/main/java/com/itvillage/section11/class05/ConcatExample02.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 
 /**
  * concat 기본 개념 예제
- *  - 파라미터로 입력된 Iterable의 Source Sequence 들을 연결해서 차례대로 emit한다.
+ *  - 파라미터로 입력된 Iterable의 Publisher Sequence 들을 연결해서 차례대로 emit한다.
  */
 public class ConcatExample02 {
     public static void main(String[] args) {
