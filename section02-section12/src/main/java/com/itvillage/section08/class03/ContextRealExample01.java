a56bd24fd4d4122facb575108d00f23f8bc219e1
hjs6877 <hjs6877@gmail.com>
2022-03-24 23:48:25 +0900

    Modify ContextRealExample01

--- section02-section12/src/main/java/com/itvillage/section08/class03/ContextRealExample01.java
+++ section02-section12/src/main/java/com/itvillage/section08/class03/ContextRealExample01.java
@@ -11,13 +11,13 @@ import reactor.util.context.Context;
 public class ContextRealExample01 {
     public static final String HEADER_NAME_AUTH_TOKEN = "authToken";
     public static void main(String[] args) {
-        Mono<String> mono = postBook(Mono.just(
-                                                new Book("abcd-1111-3533-2809",
-                                                        "Reactor의 정석",
-                                                        "Kevin")
-                                                )
-                            )
-                            .contextWrite(Context.of(HEADER_NAME_AUTH_TOKEN, "eyJhbGciOiJIUzUxMiJ9.eyJzdWI"));
+        Mono<String> mono =
+                postBook(Mono.just(
+                                    new Book("abcd-1111-3533-2809"
+                                            , "Reactor's Bible"
+                                            ,"Kevin"))
+                )
+                .contextWrite(Context.of(HEADER_NAME_AUTH_TOKEN, "eyJhbGciOiJIUzUxMiJ9.eyJzdWI"));
 
         mono.subscribe(Logger::onNext);
 
@@ -25,12 +25,11 @@ public class ContextRealExample01 {
 
     private static Mono<String> postBook(Mono<Book> book) {
         return Mono.zip(book, Mono.deferContextual(ctx -> Mono.just(ctx.get(HEADER_NAME_AUTH_TOKEN))))
-                .map(tuple -> {
-                    // HTTP POST 전송을 했다고 가정.
-
-                    String response = "POST the book(" + tuple.getT1().getBookName() + ","
-                            + tuple.getT1().getAuthor() + ") with token: " + tuple.getT2();
-                    return response;
+                .flatMap(tuple -> {
+                    String response = "POST the book(" + tuple.getT1().getBookName() +
+                            "," + tuple.getT1().getAuthor() + ") with token: " +
+                            tuple.getT2();
+                    return Mono.just(response); // HTTP POST 전송을 했다고 가정
                 });
     }
 }
