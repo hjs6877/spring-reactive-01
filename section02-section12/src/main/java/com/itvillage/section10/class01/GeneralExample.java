8cac3ef6686068dd02552480360fb0149e4f10df
hjs6877 <hjs6877@gmail.com>
2022-04-07 00:22:35 +0900

    Modify GeneralExample

--- section02-section12/src/main/java/com/itvillage/section10/class01/GeneralExample.java
+++ section02-section12/src/main/java/com/itvillage/section10/class01/GeneralExample.java
@@ -1,9 +1,6 @@
 package com.itvillage.section10.class01;
 
-import com.itvillage.utils.Logger;
-import com.itvillage.utils.TimeUtils;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
 public class GeneralExample {
     public static Flux<String> sayHelloReactor() {
