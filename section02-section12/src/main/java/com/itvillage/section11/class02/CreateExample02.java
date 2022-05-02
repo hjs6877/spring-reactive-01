f2709033755afc4b6200dd86605de6eb4602a203
hjs6877 <hjs6877@gmail.com>
2022-05-03 00:02:15 +0900

    Modify create operator's examples

--- section02-section12/src/main/java/com/itvillage/section11/class02/CreateExample02.java
+++ section02-section12/src/main/java/com/itvillage/section11/class02/CreateExample02.java
@@ -50,7 +50,7 @@ public class CreateExample02 {
 
         TimeUtils.sleep(2000L);
         priceEmitter.complete();
-        
+
         TimeUtils.sleep(100L);
     }
 }
--- section02-section12/src/main/java/com/itvillage/section11/class02/CreateExample03.java
+++ section02-section12/src/main/java/com/itvillage/section11/class02/CreateExample03.java
@@ -2,44 +2,41 @@ package com.itvillage.section11.class02;
 
 import com.itvillage.utils.Logger;
 import com.itvillage.utils.TimeUtils;
-import org.reactivestreams.Subscription;
-import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 import reactor.core.scheduler.Schedulers;
 
 /**
  * create 개념 이해 예제
- *  - Subscriber 가 request 할 경우에 next signal 이벤트를 발생하는 예제
+ *  - Subscriber가 request 할 경우에 next signal 이벤트를 발생하는 예제
  *  - Backpressure 전략을 적용하는 예제
- *  // TODO pull 방식으로 바꾸기
  */
 public class CreateExample03 {
-    public static int emitSize = 50000 ;
-    public static int count = 0;
+    public static int start = 1;
+    public static int end = 4 ;
+
     public static void main(String[] args) {
-        Logger.info("# start");
         Flux.create((FluxSink<Integer> emitter) -> {
             emitter.onRequest(n -> {
                 Logger.info("# requested: " + n);
-                TimeUtils.sleep(5L);
-                for (int i = count; i <= emitSize; i++) {
-                    count++;
+                TimeUtils.sleep(500L);
+                for (int i = start; i <= end; i++) {
                     emitter.next(i);
                 }
+                start += 4;
+                end += 4;
             });
 
             emitter.onDispose(() -> {
                 Logger.info("# clean up");
             });
-        }, FluxSink.OverflowStrategy.LATEST)
-                .subscribeOn(Schedulers.boundedElastic())
-                .publishOn(Schedulers.parallel())
+        }, FluxSink.OverflowStrategy.DROP)
+        .subscribeOn(Schedulers.boundedElastic())
+        .publishOn(Schedulers.parallel(), 2)
         .subscribe(data -> {
-            TimeUtils.sleep(10L);
             Logger.onNext(data);
         });
 
-        TimeUtils.sleep(5000L);
+        TimeUtils.sleep(3000L);
     }
 }
