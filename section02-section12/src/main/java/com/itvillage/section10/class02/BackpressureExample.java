47258cbd08cce68aab401b1a816894b9a361fe47
hjs6877 <hjs6877@gmail.com>
2023-01-26 00:42:42 +0900

    .

--- section02-section12/src/main/java/com/itvillage/section10/class02/BackpressureExample.java
+++ section02-section12/src/main/java/com/itvillage/section10/class02/BackpressureExample.java
@@ -4,7 +4,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 
 public class BackpressureExample {
-    public static Flux<Integer> generateNumber() {
+    public static Flux<Integer> generateNumberByErrorStrategy() {
         return Flux
                 .create(emitter -> {
                     for (int i = 1; i <= 100; i++) {
@@ -12,5 +12,15 @@ public class BackpressureExample {
                     }
                     emitter.complete();
                 }, FluxSink.OverflowStrategy.ERROR);
+    }
+
+    public static Flux<Integer> generateNumberByDropStrategy() {
+        return Flux
+                .create(emitter -> {
+                    for (int i = 1; i <= 100; i++) {
+                        emitter.next(i);
+                    }
+                    emitter.complete();
+                }, FluxSink.OverflowStrategy.DROP);
     }
 }
